81053407ebdcf10dd90e309c3c468f150d1db934
Anwesh43 <anweshthecool0@gmail.com>
2018-02-25 03:37:27 +0530

    using view in activity

--- app/src/main/java/ui/anwesome/com/kotlincircletocircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincircletocircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincircletocircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circletocircleview.CircleToCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleToCircleView.create(this)
     }
 }
