b3b782655729903ca287438fe25c994a3dd0b72a
Anwesh43 <anweshthecool0@gmail.com>
2018-02-25 02:57:55 +0530

    adding state class

--- circletocircleview/src/main/java/ui/anwesome/com/circletocircleview/CircleToCircleView.kt
+++ circletocircleview/src/main/java/ui/anwesome/com/circletocircleview/CircleToCircleView.kt
@@ -19,4 +19,26 @@ class CircleToCircleView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class State(var j:Int = 0, var dir:Float = 0f) {
+        var scales:Array<Float> = arrayOf(0f, 0f)
+        fun update(stopcb : () -> Unit) {
+            scales[j] += 0.1f * dir
+            if(scales[j] > 1) {
+                scales[j] = 1f
+                j++
+                if(j == scales.size) {
+                    dir = 0f
+                    j = 0
+                    stopcb()
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if(dir == 0f) {
+                dir = 1f
+                scales = arrayOf(0f, 0f)
+                startcb()
+            }
+        }
+    }
 }
