efb40388b1873506c99423d6fc9c6b21e27c5126
Anwesh43 <anweshthecool0@gmail.com>
2018-02-25 02:48:46 +0530

    adding CircleToCircleView class

--- circletocircleview/src/main/java/ui/anwesome/com/circletocircleview/CircleToCircleView.kt
+++ circletocircleview/src/main/java/ui/anwesome/com/circletocircleview/CircleToCircleView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.circletocircleview
 /**
  * Created by anweshmishra on 25/02/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class CircleToCircleView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
