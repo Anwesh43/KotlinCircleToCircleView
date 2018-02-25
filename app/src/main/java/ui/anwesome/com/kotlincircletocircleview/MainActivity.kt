0897cc30ce7471693cb6df031697eb13ad409284
Anwesh43 <anweshthecool0@gmail.com>
2018-02-25 12:25:29 +0530

    adding CircleToCircleListener in app

--- app/src/main/java/ui/anwesome/com/kotlincircletocircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincircletocircleview/MainActivity.kt
@@ -3,17 +3,22 @@ package ui.anwesome.com.kotlincircletocircleview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.circletocircleview.CircleToCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CircleToCircleView.create(this)
+        val view = CircleToCircleView.create(this)
         fullScreen()
+        view.addCircleToCircleListener({ createToast("moved left") },{ createToast("moved right") },{ createToast("can't move to left") },{ createToast("can't move to right") })
     }
 }
 fun MainActivity.fullScreen() {
     supportActionBar?.hide()
     window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
+fun MainActivity.createToast(text : String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
