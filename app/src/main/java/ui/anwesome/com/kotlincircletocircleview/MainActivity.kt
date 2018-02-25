19bc0cbda32adb4ca2387535ce2b12d5bb963b50
Anwesh43 <anweshthecool0@gmail.com>
2018-02-25 12:00:59 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/kotlincircletocircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincircletocircleview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlincircletocircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.circletocircleview.CircleToCircleView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         CircleToCircleView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
